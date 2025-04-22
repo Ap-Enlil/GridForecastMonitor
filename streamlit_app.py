6e4568f66b0dab22961779a96c6a77a2e9a7141d
Ap-Enlil <badcontest7@gmail.com>
2025-04-22 02:24:36 +0000

    plotly import

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import sys # Keep for potential future use, though not strictly necessary now
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import datetime
